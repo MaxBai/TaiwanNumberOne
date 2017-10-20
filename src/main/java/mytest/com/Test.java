8481bb35a2dc6d2fe751c266c8a74328ee8de01c
MaxBai <maxbai@eztravel.com.tw>
2017-10-20 14:52:23 +0800

    update 台灣藍波萬

--- src/main/java/mytest/com/Test.java
+++ src/main/java/mytest/com/Test.java
@@ -5,8 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("Test!!!");
-		System.out.println("xxxxx!!!");
-		System.out.println("YYYYY!!!");
+		System.out.println("台灣藍波萬");
 	}
 
 }
